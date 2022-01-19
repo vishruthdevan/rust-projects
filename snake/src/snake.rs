3b2efd94130e746241615be2e9fc6b819bcd40db
Vishruth <vishruthdevan@gmail.com>
2022-01-19 21:54:17 +0530

    Opposite function to restrict snake direction

--- snake/src/snake.rs
+++ snake/src/snake.rs
@@ -1,3 +1,25 @@
 use piston_window::types::Color;
 use piston_window::{Context, G2d};
 use std::collections::LinkedList;
+
+use draw::draw_block;
+
+const SNAKE_COLOUR: Color = [0.0, 0.80, 0.0, 1.0];
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
